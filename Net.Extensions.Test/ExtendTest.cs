64c6d2ece59034ed7c0e56f9458a024ff109d78a
Salih Keleş <keles.salih@gmail.com>
2019-03-27 18:47:04 +0300

    Dictionary extend added

---
+++ Net.Extensions.Test/ExtendTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Net.Extensions.Test
+{
+    public class ExtendTest
+    {
+        [Fact]
+        public void ExtendPropertyTest()
+        {
+            var x = new Dictionary<string, object>();
+            var result = x.Extend("a.b.c", "32");
+            x.Extend("a.b.e", "oley",false);
+        }
+
+    }
+}
--- Net.Extensions/Dictionary/DictionaryExtensions.cs
+++ Net.Extensions/Dictionary/DictionaryExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+
 namespace Net.Extensions
 {
     public static class DictionaryExtensions
@@ -7,6 +9,22 @@ namespace Net.Extensions
         {
             if (key == null || dic==null) return default(TValue);
             return dic.ContainsKey(key) ? dic[key] : default(TValue);
+        }
+        public static Dictionary<string, object> Extend(this Dictionary<string, object> dic, string path, object value, bool overrideExists = true)
+        {
+            dic = dic ?? new Dictionary<string, object>();
+            var paths = path.SplitBy(".");
+            var current = dic;
+            for (var i = 0; i < paths.Length - 1; i++)
+            {
+                var p = paths[i];
+                if (!current.ContainsKey(p) || !(current[p] is Dictionary<string, object>))
+                    current[p] = new Dictionary<string, object>();
+                current = current[p] as Dictionary<string, object>;
+            }
+            if (!overrideExists && current.ContainsKey(paths.Last())) return dic;
+            current[paths.Last()] = value;
+            return dic;
         }
        
     }
